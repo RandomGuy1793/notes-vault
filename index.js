d05b8e7836be09f67d041f621d7cc944805953ad
RandomGuy1793 <faisal.alam.1999@gmail.com>
2021-06-06 23:05:42 +0530

    added delete functionality

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function addNote() {
-    var title=document.querySelector("#create .title").value;
-    var text=document.querySelector("#create .text").value;
-    if(title.trim() || text.trim()){
-        var prev=document.querySelector("#store .row").innerHTML
-        document.querySelector("#store .row").innerHTML = prev+
-        `<div class="col-lg-2 p-4">
+    var title = document.querySelector("#create .title").value;
+    var text = document.querySelector("#create .text").value;
+    if (title.trim() || text.trim()) {
+        var prev = document.querySelector("#store .row").innerHTML
+        document.querySelector("#store .row").innerHTML = prev +
+            `<div class="col-lg-2 p-4">
             <div class="card p-3 shadow-sm">
-            <h5>`+title+`</h5>
-            <p>`+text+`</p>
+            <h5> ${title} </h5>
+            <p> ${text} </p>
+            <button class="delete" onclick="deleteNote(this)"><i class="fas fa-trash"></i></button>
             </div>
         </div>`
     }
-    document.querySelector("#create .title").value="";
-    document.querySelector("#create .text").value="";
+    document.querySelector("#create .title").value = "";
+    document.querySelector("#create .text").value = "";
 }
+
+function deleteNote(btn) {
+    btn.parentNode.parentNode.remove();
+}
+
