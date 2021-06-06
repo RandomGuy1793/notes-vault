0f78c8d0df0e3d2c247b5bde6e28b894520201be
RandomGuy1793 <faisal.alam.1999@gmail.com>
2021-06-06 20:00:31 +0530

    minor changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function addNote() {
     if(title.trim() || text.trim()){
         var prev=document.querySelector("#store .row").innerHTML
         document.querySelector("#store .row").innerHTML = prev+
-        `<div class="col-lg-3 p-4">
+        `<div class="col-lg-2 p-4">
             <div class="card p-3 shadow-sm">
             <h5>`+title+`</h5>
             <p>`+text+`</p>
