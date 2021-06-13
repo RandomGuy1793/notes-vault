594028f1f63d3a2659012fef1c370fe61fda8591
RandomGuy1793 <faisal.alam.1999@gmail.com>
2021-06-13 23:30:03 +0530

    newer notes are shown first

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function addNote() {
     var text = document.querySelector("#create .text").value;
     if (title.trim() || text.trim()) {
         var prev = document.querySelector("#store .row").innerHTML
-        document.querySelector("#store .row").innerHTML = prev +
+        document.querySelector("#store .row").innerHTML =
             `<div class="col-lg-2 p-4">
             <div class="card p-3 shadow-sm">
             <h5> ${title} </h5>
             <p> ${text} </p>
             <button class="delete" onclick="deleteNote(this)"><i class="fas fa-trash"></i></button>
             </div>
-        </div>`
+        </div>` + prev
     }
     document.querySelector("#create .title").value = "";
     document.querySelector("#create .text").value = "";
