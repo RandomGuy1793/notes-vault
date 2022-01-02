7356845d3a08693290ee0dc1ec445f2fe50afb55
Faisal Alam <faisal.alam.1999@gmail.com>
2022-01-03 00:34:33 +0530

    added edit functionality to existing notes

--- components/home.js
+++ components/home.js
@@ -1,124 +1,129 @@
-const token=sessionStorage.getItem("jwt");
-// console.log(token);
-const isSkipped=sessionStorage.getItem('skipped')
+import { makeNote, getTitleAndText, titleBuffer, textBuffer } from "./note.js";
 
-const makeNote=(title, text, _id=null)=>{
-    const div1=document.createElement('div');
-    div1.classList.add('col-lg-2', 'p-4')
+const token = sessionStorage.getItem("jwt");
+const isSkipped = sessionStorage.getItem('skipped')
 
-    const div2=document.createElement('div');
-    div2.classList.add('card', 'p-3', 'shadow-sm', 'note');
-
-    const h5=document.createElement('h5');
-    h5.innerHTML=title;
-
-    const para=document.createElement('p');
-    para.innerHTML=text;
-
-    const button=document.createElement('button');
-    button.classList.add('delete');
-    button.addEventListener('click', deleteNote);
-
-    const objIdEle=document.createElement('div');
-    objIdEle.style.display="none"
-    objIdEle.innerHTML=_id
-
-    const i=document.createElement('i');
-    i.classList.add('fas', 'fa-trash')
-
-    button.appendChild(i);
-    div2.append(h5, para, button, objIdEle);
-    div1.appendChild(div2);
-
-    let prev = document.querySelector("#store .row");
-    prev.prepend(div1);
-}
-
-const addNote=()=> {
+const addNote = () => {
     let title = document.querySelector("#create .title").value;
     let text = document.querySelector("#create .text").value;
     if (title.trim() && text.trim()) {
-        if(isSkipped) makeNote(title, text);
+        if (isSkipped) makeNote(title, text);
         else postNote(title, text);
     }
     document.querySelector("#create .title").value = "";
     document.querySelector("#create .text").value = "";
 }
 
-let addButton=document.querySelector('.add-note');
-addButton.addEventListener('click', addNote);
-
-const getNotes=async()=>{
-    const res=await fetch('http://localhost:4000/api/notes', {
+const getNotes = async () => {
+    const res = await fetch('http://localhost:4000/api/notes', {
         // mode: "no-cors",
         method: "GET",
-        headers:{
+        headers: {
             "Content-Type": "application/json",
             "x-auth-token": token
         }
     })
-    if(res.status===200){
-        const data=await res.json()
+    if (res.status === 200) {
+        const data = await res.json()
         console.log(data);
-        data.forEach((obj)=>{
+        data.forEach((obj) => {
             makeNote(obj.title, obj.text, obj._id)
         })
     }
-    
 }
 
-const postNote=async(noteTitle, noteText)=>{
-    const obj={
+const postNote = async (noteTitle, noteText) => {
+    const obj = {
         title: noteTitle,
         text: noteText
     }
-    const JsonData=JSON.stringify(obj);
+    const JsonData = JSON.stringify(obj);
     console.log(JsonData);
-    const res=await fetch('http://localhost:4000/api/notes/add', {
+    const res = await fetch('http://localhost:4000/api/notes/add', {
         // mode: "no-cors",
         method: "POST",
-        headers:{
+        headers: {
             "Content-Type": "application/json",
             "x-auth-token": token
         },
         body: JsonData
     })
-    if(res.status===200){
-        const data=await res.json();
+    if (res.status === 200) {
+        const data = await res.json();
         console.log(data);
-        const {title, text, _id}=data;
+        const { title, text, _id } = data;
         makeNote(title, text, _id)
     }
 }
 
-const deleteNote=async(e)=>{
-    const ele=e.target;
-    
-    // console.log(ele);
-    if(!isSkipped){
-        const objId=ele.parentNode.parentNode.lastChild.innerText;
+const deleteNote = async (e) => {
+    const parentOfObjIdDiv = e.target.parentNode.parentNode.parentNode.parentNode
+    if (!isSkipped) {
+        const objId = parentOfObjIdDiv.lastChild.innerText;
         console.log(objId);
-        const res=await fetch(`http://localhost:4000/api/notes/delete/${objId}`, {
+        const res = await fetch(`http://localhost:4000/api/notes/delete/${objId}`, {
             // mode: "no-cors",
             method: "DELETE",
-            headers:{
+            headers: {
                 "Content-Type": "application/json",
                 "x-auth-token": token
             }
         })
-        if(res.status!==200){
+        if (res.status !== 200) {
             return
         }
         // e.target.parentNode.parentNode.parentNode.remove()
     }
-    ele.parentNode.parentNode.parentNode.remove()
+    const ele2Remove = parentOfObjIdDiv.parentNode;
+    ele2Remove.remove()
 }
 
-const logOut=document.querySelector('.log-out');
-logOut.addEventListener('click', ()=>{
+const editNote = async (e) => {
+    const [titleEle, textEle]=getTitleAndText(e)
+    const noteTitle=titleEle.innerText
+    const noteText=textEle.innerText
+    if(!isSkipped && !(noteTitle===titleBuffer && noteText===textBuffer)){
+        const obj = {
+            title: noteTitle,
+            text: noteText
+        }
+        const parentOfObjIdDiv = titleEle.parentNode
+        const objId=parentOfObjIdDiv.lastChild.innerText
+        console.log(objId);
+        const JsonData = JSON.stringify(obj);
+        console.log(JsonData);
+        const res = await fetch(`http://localhost:4000/api/notes/edit/${objId}`, {
+            // mode: "no-cors",
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "x-auth-token": token
+            },
+            body: JsonData
+        })
+        if (res.status !== 200) {
+            titleEle.innerText=titleBuffer
+            textEle.innerText=textBuffer
+        }
+    }
+    titleEle.contentEditable=false;
+    textEle.contentEditable=false;
+    titleEle.style.backgroundColor='white'
+    textEle.style.backgroundColor='white'
+}
+
+
+
+let addButton = document.querySelector('.add-note');
+addButton.addEventListener('click', addNote);
+
+const logOut = document.querySelector('.log-out');
+logOut.addEventListener('click', () => {
     window.location.replace("../../index.html")
 })
 
-if(!isSkipped) getNotes();
+if (!isSkipped) getNotes();
+
+export {deleteNote, editNote}
 
 
---
+++ components/note.js
@@ -0,0 +1,71 @@
+import { deleteNote, editNote } from "./home.js";
+let titleBuffer, textBuffer;
+
+const makeElementWithClass = (eleType, ...classArray) => {
+    const ele = document.createElement(eleType);
+    ele.classList.add(...classArray);
+    return ele;
+}
+
+const getTitleAndText=(e)=>{
+    const parent=e.target.parentNode.parentNode.parentNode.parentNode
+    const titleEle=parent.children[0];
+    const textEle=parent.children[1];
+    console.log(titleEle);
+    return [titleEle, textEle]
+}
+
+const handleEditButton=(e)=>{
+    const [titleEle, textEle]=getTitleAndText(e);
+    const title=titleEle.innerText
+    // console.log(title);
+    const text=textEle.innerText
+    // console.log(text);
+    titleBuffer=title
+    textBuffer=text
+    titleEle.contentEditable=true;
+    textEle.contentEditable=true;
+    titleEle.style.backgroundColor='#e6e6e6'
+    textEle.style.backgroundColor='#e6e6e6'
+}
+
+const makeNote = (title, text, _id = null) => {
+    const div1 = makeElementWithClass('div', 'col-lg-2', 'p-4')
+    const div2 = makeElementWithClass('div', 'card', 'p-3', 'shadow-sm', 'note')
+    const h5 = document.createElement('h5');
+    h5.innerHTML = title;
+    const para = document.createElement('p');
+    para.innerHTML = text;
+
+    const delButton = makeElementWithClass('button', 'delete-button')
+    delButton.addEventListener('click', deleteNote);
+    const delIcon = makeElementWithClass('i', 'fas', 'fa-trash')
+    const editButton = makeElementWithClass('button', 'edit-button')
+    editButton.addEventListener('click', handleEditButton);
+    const editIcon = makeElementWithClass('i', 'fas', 'fa-edit')
+    const saveButton = makeElementWithClass('button', 'save-button')
+    saveButton.addEventListener('click', editNote);
+    const saveIcon = makeElementWithClass('i', 'fas', 'fa-save')
+    const objIdEle = document.createElement('div');
+    objIdEle.style.display = "none"
+    objIdEle.innerHTML = _id
+    const buttonsDiv = makeElementWithClass('div', 'row', 'text-center')
+    const deleteDiv = makeElementWithClass('div', 'col-4')
+    const editDiv = makeElementWithClass('div', 'col-4')
+    const saveDiv = makeElementWithClass('div', 'col-4')
+
+    delButton.appendChild(delIcon);
+    editButton.appendChild(editIcon);
+    saveButton.appendChild(saveIcon);
+    editDiv.appendChild(editButton)
+    saveDiv.appendChild(saveButton)
+    deleteDiv.appendChild(delButton)
+    buttonsDiv.append(editDiv, saveDiv, deleteDiv)
+    div2.append(h5, para, buttonsDiv, objIdEle);
+    div1.appendChild(div2);
+
+    let prev = document.querySelector("#store .row");
+    prev.prepend(div1);
+}
+
+export {makeNote, getTitleAndText, titleBuffer, textBuffer}
